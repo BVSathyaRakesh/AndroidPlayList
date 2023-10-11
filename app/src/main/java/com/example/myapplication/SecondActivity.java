5475d378cd578799253d589c2edf7159833eeccd
Sathya Rakesh <bvsrakesh.kumar@i-exceed.com>
2023-10-11 18:53:30 +0530

    Intent Changes Part1

--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -1,14 +1,39 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.app.Notification;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.net.URI;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private CardView cardWebBtn;
+    private CardView cardCameraBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        cardWebBtn = this.findViewById(R.id.cardWeb);
+        cardCameraBtn = this.findViewById(R.id.cardCamera);
+    }
+
+    public void  cardWebBtnPressed(View v){
+        Log.v("check","web clicked");
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+        Uri.parse("https://www.google.com/"));
+        startActivity(intent);
+    }
+
+    public void  cardCameraBtnPressed(View v){
+        Log.v("check","camera clicked");
     }
 }
